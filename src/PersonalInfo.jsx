6fee61003411ef981bbc3ec45c19a937a6d97e98
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-05-05 20:24:22 -0400

    Add personalInfo component with user details layout and styles

---
+++ src/PersonalInfo.jsx
@@ -0,0 +1,35 @@
+import personIcon from "./assets/icons/user-avatar.png";
+
+export function PersonalInfo() {
+  return (
+    <div className="personal-info-container form">
+      <div className="title-wrapper">
+        <img src={personIcon} alt="" />
+        <h2>Personal Details</h2>
+      </div>
+
+      <form className="personal-info-form" action="#">
+        <div className="fullName wrapper">
+          <label htmlFor="fullName">FULL NAME</label>
+          <input id="fullName" type="text" placeholder="Full Name" />
+        </div>
+        <div className="email wrapper">
+          <label htmlFor="email">EMAIL</label>
+          <input id="email" type="email" placeholder="eg. name@example.com" />
+        </div>
+        <div className="phoneNumber wrapper">
+          <label htmlFor="phoneNumber">PHONE NUMBER</label>
+          <input id="phoneNumber" type="number" placeholder="(123) 456 7890" />
+        </div>
+        <div className="address wrapper">
+          <label htmlFor="address">Address</label>
+          <textarea
+            id="address"
+            type="text"
+            placeholder="1234 Main St, Springfield, IL 62704"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,73 +6,91 @@ import defaultWorkHistoryIcon from "./assets/icons/default-suit-case.png";
 import workHistoryIcon from "./assets/icons/suit-case.png";
 import defaultSkillsIcon from "./assets/icons/default-automation.png";
 import skillsIcon from "./assets/icons/automation.png";
+import { PersonalInfo } from "./PersonalInfo";
 import { useState } from "react";
 
 export function Sidebar() {
-  const [isActive, setIsActive] = useState(null);
+  const [isActive, setIsActive] = useState(0);
+
+  function renderSection() {
+    switch (isActive) {
+      case 0:
+        return <PersonalInfo />;
+    }
+  }
 
   return (
-    <div className="main-sidebar">
-      <div className="category-section">
-        <div className="section-info">
-          <p>Resume Sections</p>
-          <p className="complete-p">Complete your profile</p>
+    <div className="main-wrapper">
+      <div className="main-sidebar">
+        <div className="category-section">
+          <div className="section-info">
+            <p>Resume Sections</p>
+            <p className="complete-p">Complete your profile</p>
+          </div>
+          <div className="category-wrapper">
+            <div
+              className={
+                isActive === 0
+                  ? "personal-info-wrapper active"
+                  : "personal-info-wrapper"
+              }
+              onClick={() => {
+                setIsActive(0);
+              }}
+            >
+              <img
+                src={isActive === 0 ? personalIcon : defaultPersonalIcon}
+                alt=""
+              />
+              <p>Personal Info</p>
+            </div>
+            <div
+              className={
+                isActive === 1
+                  ? "education-wrapper active"
+                  : "education-wrapper"
+              }
+              onClick={() => setIsActive(1)}
+            >
+              <img
+                src={isActive === 1 ? educationIcon : defaultEducationIcon}
+                alt=""
+              />
+              <p>Education</p>
+            </div>
+            <div
+              className={
+                isActive === 2
+                  ? "work-history-wrapper active"
+                  : "work-history-wrapper"
+              }
+              onClick={() => setIsActive(2)}
+            >
+              <img
+                src={isActive === 2 ? workHistoryIcon : defaultWorkHistoryIcon}
+                alt=""
+              />
+              <p>Work History</p>
+            </div>
+            <div
+              className={
+                isActive === 3 ? "skills-wrapper active" : "skills-wrapper"
+              }
+              onClick={() => setIsActive(3)}
+            >
+              <img
+                src={isActive === 3 ? skillsIcon : defaultSkillsIcon}
+                alt=""
+              />
+              <p>Skills</p>
+            </div>
+          </div>
         </div>
-        <div className="category-wrapper">
-          <div
-            className={
-              isActive === 0
-                ? "personal-info-wrapper active"
-                : "personal-info-wrapper"
-            }
-            onClick={() => setIsActive(0)}
-          >
-            <img
-              src={isActive === 0 ? personalIcon : defaultPersonalIcon}
-              alt=""
-            />
-            <p>Personal Info</p>
-          </div>
-          <div
-            className={
-              isActive === 1 ? "education-wrapper active" : "education-wrapper"
-            }
-            onClick={() => setIsActive(1)}
-          >
-            <img
-              src={isActive === 1 ? educationIcon : defaultEducationIcon}
-              alt=""
-            />
-            <p>Education</p>
-          </div>
-          <div
-            className={
-              isActive === 2
-                ? "work-history-wrapper active"
-                : "work-history-wrapper"
-            }
-            onClick={() => setIsActive(2)}
-          >
-            <img
-              src={isActive === 2 ? workHistoryIcon : defaultWorkHistoryIcon}
-              alt=""
-            />
-            <p>Work History</p>
-          </div>
-          <div
-            className={
-              isActive === 3 ? "skills-wrapper active" : "skills-wrapper"
-            }
-            onClick={() => setIsActive(3)}
-          >
-            <img src={isActive === 3 ? skillsIcon : defaultSkillsIcon} alt="" />
-            <p>Skills</p>
-          </div>
+        <div className="button-wrapper">
+          <button className="preview-btn">Preview Resume</button>
         </div>
       </div>
-      <div className="button-wrapper">
-        <button className="preview-btn">Preview Resume</button>
-      </div>
+      {renderSection()}
     </div>
   );
 }
